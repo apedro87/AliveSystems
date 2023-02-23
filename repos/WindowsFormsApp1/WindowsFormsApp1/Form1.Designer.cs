8b637e75a5a56c5dbed012b3e61850281e6090be
Adriano Pedro <105020332+apedro87@users.noreply.github.com>
2023-02-23 00:46:42 +0000

    Add files via upload

---
+++ repos/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexecutar = new System.Windows.Forms.Button();
+            this.lstviewdados = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtHotel = new System.Windows.Forms.TextBox();
+            this.txtdescricao = new System.Windows.Forms.TextBox();
+            this.txtip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.btnRemoveLinha = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnexecutar
+            // 
+            this.btnexecutar.Location = new System.Drawing.Point(398, 23);
+            this.btnexecutar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnexecutar.Name = "btnexecutar";
+            this.btnexecutar.Size = new System.Drawing.Size(116, 33);
+            this.btnexecutar.TabIndex = 0;
+            this.btnexecutar.Text = "Executar";
+            this.btnexecutar.UseVisualStyleBackColor = true;
+            this.btnexecutar.Click += new System.EventHandler(this.Executar_Click);
+            // 
+            // lstviewdados
+            // 
+            this.lstviewdados.FullRowSelect = true;
+            this.lstviewdados.GridLines = true;
+            this.lstviewdados.HideSelection = false;
+            this.lstviewdados.Location = new System.Drawing.Point(10, 11);
+            this.lstviewdados.Margin = new System.Windows.Forms.Padding(2);
+            this.lstviewdados.Name = "lstviewdados";
+            this.lstviewdados.Size = new System.Drawing.Size(384, 320);
+            this.lstviewdados.TabIndex = 1;
+            this.lstviewdados.UseCompatibleStateImageBehavior = false;
+            this.lstviewdados.View = System.Windows.Forms.View.Details;
+            this.lstviewdados.SelectedIndexChanged += new System.EventHandler(this.Lstviewdados_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 337);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Adicionar Linha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtHotel
+            // 
+            this.txtHotel.Location = new System.Drawing.Point(57, 368);
+            this.txtHotel.Margin = new System.Windows.Forms.Padding(2);
+            this.txtHotel.Name = "txtHotel";
+            this.txtHotel.Size = new System.Drawing.Size(76, 20);
+            this.txtHotel.TabIndex = 3;
+            // 
+            // txtdescricao
+            // 
+            this.txtdescricao.Location = new System.Drawing.Point(57, 391);
+            this.txtdescricao.Margin = new System.Windows.Forms.Padding(2);
+            this.txtdescricao.Name = "txtdescricao";
+            this.txtdescricao.Size = new System.Drawing.Size(76, 20);
+            this.txtdescricao.TabIndex = 4;
+            // 
+            // txtip
+            // 
+            this.txtip.Location = new System.Drawing.Point(56, 414);
+            this.txtip.Margin = new System.Windows.Forms.Padding(2);
+            this.txtip.Name = "txtip";
+            this.txtip.Size = new System.Drawing.Size(76, 20);
+            this.txtip.TabIndex = 5;
+            this.txtip.TextChanged += new System.EventHandler(this.txtip_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 371);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hotel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 393);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Descrição";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 414);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "IP";
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(398, 73);
+            this.btnParar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(116, 33);
+            this.btnParar.TabIndex = 9;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.BtnParar_Click);
+            // 
+            // btnRemoveLinha
+            // 
+            this.btnRemoveLinha.Location = new System.Drawing.Point(103, 337);
+            this.btnRemoveLinha.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemoveLinha.Name = "btnRemoveLinha";
+            this.btnRemoveLinha.Size = new System.Drawing.Size(99, 24);
+            this.btnRemoveLinha.TabIndex = 10;
+            this.btnRemoveLinha.Text = "Remover Linha";
+            this.btnRemoveLinha.UseVisualStyleBackColor = true;
+            this.btnRemoveLinha.Click += new System.EventHandler(this.BtnRemoveLinha_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(542, 20);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(74, 36);
+            this.btnsave.TabIndex = 11;
+            this.btnsave.Text = "Guardar";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.Btnsave_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 445);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnRemoveLinha);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtip);
+            this.Controls.Add(this.txtdescricao);
+            this.Controls.Add(this.txtHotel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstviewdados);
+            this.Controls.Add(this.btnexecutar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnexecutar;
+        private System.Windows.Forms.ListView lstviewdados;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtHotel;
+        private System.Windows.Forms.TextBox txtdescricao;
+        private System.Windows.Forms.TextBox txtip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Button btnRemoveLinha;
+        private System.Windows.Forms.Button btnsave;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ repos/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using AliveSystems.Properties;
+using System.IO;
+using System.Net.Mail;
+using System.Threading;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DateTime now = DateTime.Now;
+        StringCollection myCol = new StringCollection();
+        ListViewItem item = new ListViewItem();
+        bool pingable = false;
+        Ping pinger = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public StringCollection GetSettings()
+        {
+            
+            myCol = Settings.Default.ListaIps;
+            return myCol;
+            
+        }
+
+        public void SaveSettings()
+        {
+
+            myCol = Settings.Default.ListaIps;
+            Settings.Default.Save();
+
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtip.Text != "")
+            {
+                
+                ListViewItem item = new ListViewItem(txtHotel.Text);
+                item.SubItems.Add(txtdescricao.Text);
+                item.SubItems.Add(txtip.Text);
+                lstviewdados.Items.Add(item);
+                txtHotel.Clear();
+                txtdescricao.Clear();
+                txtip.Clear();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSettings();
+
+            lstviewdados.View = View.Details;
+            lstviewdados.Columns.Add("Hotel");
+            lstviewdados.Columns.Add("Descrição");
+            lstviewdados.Columns.Add("IP");
+            
+
+            for (int i = 0; i < myCol.Count; i=i+3)
+            {
+                
+                lstviewdados.Items.Add(
+                new ListViewItem(new[]
+                {
+
+                myCol[i],
+                myCol[i + 1],
+                myCol[i + 2],
+                }));
+            }
+
+
+
+            for (int i = 0; i < lstviewdados.Columns.Count; i++)
+               {
+                  lstviewdados.Columns[i].Width = -2;
+               }
+            
+
+        }
+
+        public void Lstviewdados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Processamento()
+        {
+
+            
+        }
+        private void Executar_Click(object sender, EventArgs e)
+        {
+            int contaLista = 0;
+            //ListViewItem item = new ListViewItem();
+            int[] array1;
+            contaLista = lstviewdados.Items.Count;
+            array1 = new int[contaLista];
+            if (lstviewdados.Items.Count != 0)
+            {
+
+                pinger = new Ping();
+                for (int x = 0; x <= contaLista - 1; x++)
+                {
+                    try
+                    {
+                        //MessageBox.Show(lstviewdados.Items[x].SubItems[2].Text);
+                        PingReply reply = pinger.Send(lstviewdados.Items[x].SubItems[2].Text);
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            pingable = true;
+                            //MessageBox.Show(lstviewdados.Items[x].SubItems[2].Text + " está a pingar");
+                            lstviewdados.Items[x].UseItemStyleForSubItems = false;
+                            lstviewdados.Items[x].SubItems[2].ForeColor = Color.Green;
+                        }
+                        //pingable = reply.Status == IPStatus.Success;
+
+                    }
+                    catch (PingException)
+                    {
+                        //MessageBox.Show(lstviewdados.Items[x].SubItems[2].Text + " não está a pingar");
+                        lstviewdados.Items[x].UseItemStyleForSubItems = false;
+                        lstviewdados.Items[x].SubItems[2].ForeColor = Color.Red;
+                        pingable = false;
+                        /* MailMessage mail = new MailMessage();
+                         SmtpClient SmtpServer = new SmtpClient("smtp.office365.com");
+
+                         mail.From = new MailAddress("");
+                         mail.To.Add("");
+                         mail.Subject = lstviewdados.Items[x].SubItems[1].Text + " " + lstviewdados.Items[x].SubItems[0].Text + " - Inactivo";
+                         mail.Body = now.ToString("F") + "\n\n Estado: Sem Resposta";
+
+                         SmtpServer.Port = 587;
+                         SmtpServer.Credentials = new System.Net.NetworkCredential("", "");
+                         SmtpServer.EnableSsl = true;
+
+                         SmtpServer.Send(mail); */
+                    }
+                }
+            }
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+         
+
+        }
+
+        private void BtnParar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRemoveLinha_Click(object sender, EventArgs e)
+        {
+            for (int i = lstviewdados.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstviewdados.Items[i].Selected)
+                {
+                    lstviewdados.Items[i].Remove();
+                }
+            }
+        }
+
+        private void Btnsave_Click(object sender, EventArgs e)
+        {
+            int contaLista = 0;
+            myCol.Clear();
+             contaLista = lstviewdados.Items.Count;
+            for (int x = 0; x <= contaLista - 1; x++)
+                for (int y = 0; y <= 2; y++)
+                {
+                  myCol.Add(lstviewdados.Items[x].SubItems[y].ToString().Substring(18).Replace("}", ""));
+                }
+
+            SaveSettings();
+
+            //int contaLista = 0;
+
+            //FileStream file = new FileStream(@"C:\Users\adriano.pedro\source\repos\WindowsFormsApp1\WindowsFormsApp1\teste.txt", FileMode.Create, FileAccess.Write);
+            //StreamWriter sw = new StreamWriter(file);
+            //contaLista = lstviewdados.Items.Count;
+            //for (int x = 0; x <= contaLista - 1; x++)
+            //    for (int y = 0; y <= 2; y++)
+            //    {
+            //        sw.WriteLine(lstviewdados.Items[x].SubItems[y].ToString().Substring(18).Replace("}",""));
+            //    }
+
+            //sw.Close();
+            //file.Close();
+
+        }
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void txtip_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
